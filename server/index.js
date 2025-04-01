2de7bb53813bc1a5b061df27af2e5740ba045965
Amal <amaljonzn0@gmail.com>
2025-04-01 23:31:04 +0530

    initial -6

--- server/index.js
+++ server/index.js
@@ -1,47 +1,41 @@
-const express=require('express')
-const cookieparser=require('cookie-parser')
-// const app=express()
-const cors=require("cors")
-const dotenv=require('dotenv')
-// require('dotenv').config()
-const connectDB=require('./src/config/db')
-const userRoute=require('./src/Routes/userroute')
-const adminRoute=require('./src/Routes/adminroute')
-const eventRoute=require('./src/Routes/eventRoute')
-const ticketRoute=require('./src/Routes/ticketroute')
-const paymentRoute=require('./src/Routes/paymentRoute')
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+const userRoute = require('./src/Routes/userroute');
+const adminRoute = require('./src/Routes/adminroute');
+const eventRoute = require('./src/Routes/eventRoute');
+const ticketRoute = require('./src/Routes/ticketroute');
+const paymentRoute = require('./src/Routes/paymentRoute');
 
-dotenv.config()
-const app=express()
-connectDB()
+dotenv.config();
+const app = express();
+connectDB();
 
-
-
-app.use(express.json())
-app.use(cookieparser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
-}))
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true
+}));
 
-app.get('/',(req,res)=>{
-    res.send("welcome")
-})
+app.get('/', (req, res) => {
+    res.send("Welcome");
+});
 
-app.use('/api/user',userRoute)
-app.use('/api/admin',adminRoute)
-app.use('/api/events',eventRoute)
-app.use('/api/tickets',ticketRoute)
-app.use('/api/payment',paymentRoute)
+app.use('/api/user', userRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/events', eventRoute);
+app.use('/api/tickets', ticketRoute);
+app.use('/api/payment', paymentRoute);
 
+const PORT = process.env.PORT || 3000;
 
-
-const PORT=process.env.PORT || 3000;
-
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = app; // Exports the app to be used by Vercel
-// }else {
- app.listen(PORT,()=>{
-    console.log(`server listening to port ${PORT}`)
- })
-//  }
+if (process.env.NODE_ENV === 'production') {
+    module.exports = app; // For Vercel
+} else {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
